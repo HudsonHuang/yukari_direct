953f4a9d890d8264885c7308d11f44ea511931b3
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2018-04-27 00:28:49 +0900

    can record

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="hello">
     hello hiho!
+    <button @click="startRecord">start!</button>
+    <button @click="stopRecord">stop!</button>
+    <audio controls id="audio"></audio>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  data () {
+    return {
+      stream: null,
+      mediaRecorder: null
+    }
+  },
+
+  mounted () {
+    navigator.mediaDevices.getUserMedia({audio: true, video: false})
+      .then((stream) => {
+        this.stream = stream
+        this.mediaRecorder = new MediaRecorder(stream)
+        this.mediaRecorder.ondataavailable = this.onFinishedReocrd
+      })
+  },
+
+  methods: {
+    startRecord () {
+      this.mediaRecorder.start()
+    },
+
+    stopRecord () {
+      this.mediaRecorder.stop()
+    },
+
+    onFinishedReocrd (event) {
+      const audio = document.getElementById('audio')
+      audio.setAttribute('controls', '')
+      const audioURL = window.URL.createObjectURL(event.data)
+      audio.src = audioURL
+    }
+  }
 }
 </script>
